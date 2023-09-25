d134449a4aaf47b2c52c335f754c12b37c9f7613
ValeriyKliuk <valeriy.kliuk@gmail.com>
2023-09-25 10:38:34 -0400

    Added Xcode's initial SwiftUI project

---
+++ Catalogue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Catalogue
+//
+//  Created by Valeriy Kliuk on 2023-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Catalogue!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
